c0e0c76a25656444f87e0dad1bbc72678781f39e
Melvin Colmenares <lmacml@MacBook-Air-de-Melvin.local>
2026-03-24 16:09:20 -0400

    Fix: Bugs and errors fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { databaseConfig } from 'src/config/database.config';
 import { IamModule } from 'src/contexts/iam/infrastructure/iam.module';
+import environment from 'src/config/environment.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [environment],
+    }),
     TypeOrmModule.forRootAsync({ useFactory: databaseConfig }),
     IamModule,
   ],
--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 interface Environment {
   REDIS_URL?: string;
   REDIS_HOST: string;
